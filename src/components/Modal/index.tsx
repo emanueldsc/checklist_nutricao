8f233ec69ccab14dc8e26cf8deedbc959d26977e
Emanuel Douglas Sousa Costa <emanuel.douglas.sc@gmail.com>
2020-11-05 15:01:58 -0300

    correção do build

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,5 @@
-import { Alert } from './alert';
 import { Modal } from './modal';
 import { Carregando } from './carregando';
 
-export { Modal, Alert, Carregando };
+export { Modal, Carregando };
 
